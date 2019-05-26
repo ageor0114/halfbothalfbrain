8f9e828d1b663e52bb6138fe154cbd664dd9d5ba
gmoon1117 <30319741+gmoon1117@users.noreply.github.com>
2019-05-25 20:45:11 -0400

    stuff

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+const shelterBox={
+  padding: '1em 2em',
+  margin: '5em',
+  fontFamily: 'Lora',
+  textDecoration: 'none',
+}
 
 class GamePage extends React.Component{
   state = {
@@ -77,7 +86,27 @@ class GamePage extends React.Component{
       <p>{this.state.story}</p>
       {this.state.showButton && <button onClick={this.onClickEnd}>End Game</button>}
     </div>}
-    {this.state.isOver && <div>Thanks for playing, hope you had fun!</div>}
+    {this.state.isOver &&
+      <div>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <p>Thanks for playing, hope you had fun!</p>
+
+      <Paper style={shelterBox} elevation={5}>
+        <Typography variant="headline" gutterBottom>
+          <u><b>Here's the final story:</b></u>
+        </Typography>
+        <br/>
+        <Typography variant="subheading" gutterBottom>
+        <div id="finalStory">{this.state.story}</div>
+        </Typography>
+    </Paper>
+      </div>}
 
 	    </div>
 	)
