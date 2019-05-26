ee744e296bb21aa86f7ef982337ddf6df5ed9ffa
Aaron Lee <aaronwlee@wifi-64-187-253-5.nyit.edu>
2019-05-25 20:24:14 -0400

    hi

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,14 +1,5 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-
-const shelterBox={
-  padding: '1em 2em',
-  margin: '5em',
-  fontFamily: 'Lora',
-  textDecoration: 'none',
-}
 
 class GamePage extends React.Component{
   state = {
@@ -69,13 +60,7 @@ class GamePage extends React.Component{
 		{!this.state.isOver &&
       <div>
         <p>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-  		    Welcome to the game page!
+  		    Welcome to the game!
   		</p>
   		<form onSubmit={this.handleSubmit}>
   			<input type="text"
@@ -87,29 +72,7 @@ class GamePage extends React.Component{
       <p>{this.state.story}</p>
       {this.state.showButton && <button onClick={this.onClickEnd}>End Game</button>}
     </div>}
-    {this.state.isOver &&
-      <div>
-
-      <p>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      Thanks for playing, hope you had fun!
-      </p>
-      <Paper style={shelterBox} elevation={5}>
-        <Typography variant="headline" gutterBottom>
-          <u><b>Here's the Final Story:</b></u>
-        </Typography>
-        <br/>
-        <Typography variant="subheading" gutterBottom>
-  {this.state.story}
-        </Typography>
-    </Paper>
-      </div>}
+    {this.state.isOver && <div>Thanks for playing, hope you had fun!</div>}
 
 	    </div>
 	)
